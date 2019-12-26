1058ac335b0fda2191d9e799144028cce457c8cb
Or Bin <orbin50@gmail.com>
2019-12-26 11:43:07 +0200

    Added a basic loading indicator and an initial version of displaying search results

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const API_ADDRESS = 'http://localhost:8080';
 
 class App extends Component {
 
-  state = { sources: {} }
+  state = { sources: {}, isLoading: false, searchResults: [] }
 
   componentDidMount() {
     this.fetchSupportedSources();
@@ -19,19 +19,42 @@ class App extends Component {
       .then(json => this.setState({sources: json}));
   }
 
-  searchPackage = (packageQuery, source, os, arch) => {
-    console.log(packageQuery);
-    console.log(source);
-    console.log(os);
-    console.log(arch);
+  searchPackage = (packageQuery, source, os, architecture) => {
+    this.setState({ isLoading: true });
+    fetch(`${API_ADDRESS}/search`, {
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        source,
+        os,
+        architecture,
+        "packages": [
+          packageQuery
+        ]
+      }),
+    }).then(response => response.json())
+      .then(json => this.setState({ searchResults: json }))
+      .then(() => this.setState({ isLoading: false }));
   };
 
   render() {
+    const loadingBar =  this.state.isLoading ? (
+      <div>LOADING</div>
+    ) : null;
+
     return (
       <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
         <div className="page-content">
           <Search sources={this.state.sources} searchPackage={this.searchPackage}/>
+        </div>
+        { loadingBar }
+        <div>
+        {
+          this.state.searchResults.map(result => (
+            <div key={result.nevraString}>{result.nevraString}</div>
+          ))
+        }
         </div>
       </div>
     );
